01f848224789c51131501ec9fb03d4aace70d6b7
kerimoff <kerimov.nurlan@gmail.com>
2019-12-14 16:42:05 +0200

    added todays weekday test

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -6,6 +6,8 @@ describe('Check date of today', () => {
     let today_date = new Date();
 
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',	'November', 'December'];
+    let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    
     function uppercase(value){
         return value.toUpperCase()
     }
@@ -13,15 +15,22 @@ describe('Check date of today', () => {
         return value.slice(0,3)
     }
 
-    it('Checks defualt shown year is todays year', () => {
+    it('Checks default shown year is todays year', () => {
         const years_text = wrapper.find('.date div h4:last-of-type').text();
         const todays_year = today_date.getFullYear().toString()
         expect(years_text).toEqual(todays_year);
     })
 
-    it('Checks defualt shown year is todays month', () => {
+    it('Checks default shown month is todays month', () => {
         const month_text = wrapper.find('.date div h4:first-of-type').text();
         const todays_month = abbreviate(uppercase(months[today_date.getMonth()]))
         expect(month_text).toEqual(todays_month);
+    })
+
+    it('Checks default shown weekday is todays weekday', () => {
+        // TODO: Ask Tsotne why wrapper.find('.container h4').text() does not work
+        const weekday_text = wrapper.findAll('.container h4').at(2).text();
+        const todays_weekday = uppercase(weekdays[today_date.getDay()])
+        expect(weekday_text).toEqual(todays_weekday);
     })
 });
