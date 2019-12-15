0667a6a5104dd1eab87322940d74f8c18fafcae0
codeforphilosophy <sariyya.abdullayeva@gmail.com>
2019-12-15 15:12:06 +0200

    added test for checking item's done property

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils'
 import List from "../../src/components/List.vue";
+import Item from "../../src/models/Item"
 
 describe('List content is empty', () => {
     const wrapper = mount(List, {
@@ -24,5 +25,20 @@ describe('List content is NOT empty', () => {
     it('Check text when to do list is NOT empty', () => {
         const h3 = wrapper.find('h3');
         expect(h3.exists()).toBe(false);
+    })
+});
+
+describe('items done property is set correctly', () => {
+    let fakeItem = new Item("fakeContent")
+    const wrapper = mount(List, {
+        propsData: {
+            list: [fakeItem]
+        }
+    })
+
+    it('Check "done" property of item when marked as done', async()=> {
+        wrapper.find('.container div span').trigger('click')
+        await wrapper.vm.$nextTick()
+        expect(wrapper.props('list')[0].done).toBe(true)
     })
 });
